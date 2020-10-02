0fe14c4fff17c10d9f64f0298e80b5c3f75229e2
nachoman-iq <ignacio.quiroga@gmail.com>
2020-10-02 07:49:36 -0300

    Mercado Liebre mejora push

--- Modulo5/Modulo5-Ej4Crud/MlibreM5/src/controllers/mainController.js
+++ Modulo5/Modulo5-Ej4Crud/MlibreM5/src/controllers/mainController.js
@@ -6,23 +6,22 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
-
-
 const controller = {
 	index: (req, res) => {
-/*	var prodVisted; //probar si pudo pasar estas variables declaradas
-	var prodEnVenta;
-				for (var i = 0; i < products.length; i++) {
+		var prodVisited = [];
+		var prodEnVenta = [];
+		for (var i = 0; i < products.length; i++) {
+           //armo un if por cada categoria por si en un futuro debiera suamr una
 			if (products[i].category == "visited") {
-			prodVisited = products[i];
-			} else {
-			prodEnVenta = products[i];
-			}
+				prodVisited.push(products[i]);
+			};
 			
-		};*/
-		//	res.render("misProductos", { productosVisitados: productosVisitados, productosEnVenta: productosEnVenta })
-		//	res.render('index',{prodVisited:prodVisited})		*/
-		res.render('index', { products,toThousand})
+			if (products[i].category == "in-sale") {
+				prodEnVenta.push(products[i]);
+			};
+		};
+		res.render('index',{prodVisited:prodVisited,prodEnVenta:prodEnVenta,toThousand}) //paso a  la vista los parametros que necesito		
+		//res.render('index', { products, toThousand }) // CUANDO LO PASO ASI "SI" FUNCIONA.Y EL IF LO HAGO EN LA VISTA
 	},
 	search: (req, res) => {
 		res.render('results')
